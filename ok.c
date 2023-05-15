348bcd06e1d0b2422525df0f7f79c6e3637fdb2e
TaisaLima <107267331+TaisaLima@users.noreply.github.com>
2023-05-15 06:06:05 -0300

    Update ok.c

--- ok.c
+++ ok.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 100
+#define MAX_CAPACITY 100
 
 typedef struct {
-    char nome[100];
-    int prioridade;
+    char name[100];
+    int priority;
 } QueueItem;
 
 typedef struct {
-    QueueItem items[MAX];
+    QueueItem items[MAX_CAPACITY];
     int capacity;
     int size;
 } PriorityQueue;
@@ -19,19 +19,18 @@ void initializePriorityQueue(PriorityQueue* queue, int capacity) {
     queue->size = 0;
 }
 
-void enqueue(PriorityQueue* queue, char* nome, int priority) {
-    
-        if (queue->size == queue->capacity) {
+void enqueue(PriorityQueue* queue, char* name, int priority) {
+    if (queue->size == queue->capacity) {
         printf("Não conseguimos atender mais ninguém\n");
         return;
-        }
+    }
     
     QueueItem item;
-    strcpy(item.nome, nome);
-    item.prioridade = priority;
-
+    strcpy(item.name, name);
+    item.priority = priority;
+    
     int i = queue->size - 1;
-    while (i >= 0 && queue->items[i].prioridade < priority) {
+    while (i >= 0 && queue->items[i].priority < priority) {
         queue->items[i + 1] = queue->items[i];
         i--;
     }
@@ -42,58 +41,58 @@ void enqueue(PriorityQueue* queue, char* nome, int priority) {
 
 char* dequeue(PriorityQueue* queue) {
     if (queue->size == 0) {
-        printf("Não há ninguém na fila\n");
+        printf("Priority queue is empty\n");
         return NULL;
     }
     
-    char* nome = queue->items[queue->size - 1].nome;
+    char* name = queue->items[queue->size - 1].name;
     queue->size--;
-    return nome;
+    return name;
 }
 
 void printPriorityQueue(PriorityQueue* queue) {
-    printf("Ordem de chamada: \n");
+    printf("Priority Queue:\n");
     printf("---------------\n");
     
     if (queue->size == 0) {
-        printf("Vazia!\n");
+        printf("Empty\n");
         return;
     }
     
     for (int i = 0; i < queue->size; i++) {
-        printf("nome: %s, Prioridade: %d, Tempo de espera: %d min \n", queue->items[i].nome, queue->items[i].prioridade, i * 30 );
+        printf("Name: %s, Priority: %d\n", queue->items[i].name, queue->items[i].priority);
     }
 }
 
 int main() {
-    int capacidade, prioridade;
-    char nome[100];
-    
-    printf("Insira a quantidade de fichas que irão ser dadas na recepção: ");
-    scanf("%d", &capacidade);
+    int capacity;
+    printf("Enter the capacity of the priority queue: ");
+    scanf("%d", &capacity);
     getchar();  // Consume the newline character
     
     PriorityQueue queue;
-    initializePriorityQueue(&queue, capacidade);
+    initializePriorityQueue(&queue, capacity);
     
-     while (1) {
-        printf("insira o nome do paciente e seu nível de prioridade (ou pressione x para sair do menu): ");
-        scanf("%s", nome);
-        if (strcmp(nome, "x") == 0) {
+    char name[100];
+    int priority;
+    
+    while (1) {
+        printf("Enter a name and priority (or 'quit' to exit): ");
+        scanf("%s", name);
+        if (strcmp(name, "quit") == 0) {
             break;
         }
-        scanf("%d", &prioridade);
+        scanf("%d", &priority);
         getchar();  // Consume the newline character
-
-        dequeue(&queue);
-        enqueue(&queue, nome, prioridade);
-        printPriorityQueue(&queue); 
+        
+        enqueue(&queue, name, priority);
+        printPriorityQueue(&queue);
     }
     
-    printf("\nEsvaziando \n");
+    printf("\nDequeuing names in priority order:\n");
     while (queue.size > 0) {
-        char* nome = dequeue(&queue);
-        printf("%s\n", nome);
+        char* name = dequeue(&queue);
+        printf("%s\n", name);
         printPriorityQueue(&queue);
     }
     
